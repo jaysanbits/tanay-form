d1d8012cf0d30c75f0294099204ab07ebbbbd0be
Sanjay K <136778371+jaysanbits@users.noreply.github.com>
2025-05-01 00:04:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/submit', (req, res) => {
 const whatsappNumber = '917416161603';
 const message = encodeURIComponent(`Hello, here are my details.\nName:${fullName}\nPhone Number:${phoneNumber}\nAddress:${address}`);
 const whatsappLink = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${message}`;
-    res.status(307).redirect(whatsappLink);
+    res.status(200).json({redirectUrl: whatsappUrl });
 });
 
 app.listen(port, () => {
