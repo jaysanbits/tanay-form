01a490acc2fcd4a6b2d71a71643366cbbdce3723
Sanjay K <136778371+jaysanbits@users.noreply.github.com>
2025-04-30 23:43:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,9 +29,21 @@ app.post('/submit', (req, res) => {
         return res.send('<h2>Submissions are closed. You cannot submit the form anymore.</h2>');
     }
 
-    const whatsappNumber = '1234567890'; // Change this to your WhatsApp number
-    const redirectUrl = `https://wa.me/${whatsappNumber}`;
-    res.redirect(redirectUrl);
+    let {
+  fullName,
+  phoneNumber,
+  address
+}= req.body;
+
+    if(!fullName || !phoneNumber || !address){
+        return res.send('<h2>All fields are required.</h2>');
+    } 
+    
+
+const whatsappNumber = '917416161603';
+const message = encodeURIComponent(`Hello, here are my details.\nName:${fullName}\nPhone Number:${phoneNumber}\nAddress:${address}`);
+const whatsappLink = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${message}`;
+    res.status(307).redirect(whatsappLink);
 });
 
 app.listen(port, () => {
